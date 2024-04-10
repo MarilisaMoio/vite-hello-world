baa9957dffa984671a7591b3bd35122ae3cc7316
Marilisa Moio <moiomarilisa@gmail.com>
2024-04-10 13:52:57 +0200

    feat: add title with Vite

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,17 @@
-<script setup>
+<script>
+	import AppTitle from "./components/AppTitle.vue";
 
+	export default {
+		components: {
+			AppTitle
+		}
+	};
 </script>
 
 <template>
+	<main>
+		<AppTitle></AppTitle>
+	</main>
 </template>
 
 <style scoped>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -1,11 +1,18 @@
-<script setup>
-
+<script>
+	export default{
+		name: "AppTitle"
+	}
 </script>
 
 <template>
-  
+	<h1>Ciaoo Vite!</h1>
 </template>
 
 <style scoped>
-
+	h1{
+		font-size: 3rem;
+		color: blue;
+		text-shadow: 1px 1px 2px black;
+		text-align: center;
+	}
 </style>
